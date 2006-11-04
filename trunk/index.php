612c92f848323fa0d5a43396fd5a2430b09cae7c
rob <rob@f4f260f4-61fb-4e1b-9bfc-0e9184e17736>
2006-11-04 15:12:35 +0000

    Update for version 0.2 of the Zend Framework

--- trunk/index.php
+++ trunk/index.php
@@ -3,7 +3,8 @@ error_reporting(E_ALL|E_STRICT);
 date_default_timezone_set('Europe/London');
 
 set_include_path('.' . PATH_SEPARATOR . './library/'
-	 . PATH_SEPARATOR . './application/models');
+	 . PATH_SEPARATOR . './application/models'
+	 . get_include_path());
 include "Zend.php";
 
 Zend::loadClass('Zend_Controller_Front');
@@ -20,7 +21,7 @@ Zend::register('post', new Zend_Filter_Input($_POST));
 Zend::register('get', new Zend_Filter_Input($_GET));
 
 // load configuration
-$config = new Zend_Config(Zend_Config_Ini::load('./application/config.ini', 'general'));
+$config = new Zend_Config_Ini('./application/config.ini', 'general');
 Zend::register('config', $config);
 
 // setup database
@@ -33,10 +34,11 @@ $view->setScriptPath('./application/views');
 Zend::register('view', $view);
 
 // setup controller
-$route = new Zend_Controller_RewriteRouter();
-$route->addRoute('edit', ':controller/:action/id/:id', array('controller' => 'index', 'action' => 'index'));
+$router = new Zend_Controller_RewriteRouter();
+$baseUrl = substr($_SERVER['PHP_SELF'], 0, strpos($_SERVER['PHP_SELF'], '/index.php'));
+$router->setRewriteBase($baseUrl);
 $controller = Zend_Controller_Front::getInstance();
-$controller->setRouter($route);
+$controller->setRouter($router);
 
 // run!
 $controller->run('./application/controllers'); 
