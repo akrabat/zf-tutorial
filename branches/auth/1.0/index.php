40fa5905be8c9db4e35a4c4f71d6b56446e919dc
rob <rob@f4f260f4-61fb-4e1b-9bfc-0e9184e17736>
2007-03-25 19:46:47 +0000

    branch for v1.0 of the auth tutorial

---
+++ branches/auth/1.0/index.php
@@ -0,0 +1,36 @@
+<?php
+error_reporting(E_ALL|E_STRICT);
+date_default_timezone_set('Europe/London');
+
+set_include_path('.' . PATH_SEPARATOR . './library/'
+	 . PATH_SEPARATOR . './application/models/'
+	 . PATH_SEPARATOR . './application/lib/'
+	 . PATH_SEPARATOR . get_include_path());
+include "Zend/Loader.php";
+
+Zend_Loader::loadClass('Zend_Controller_Front');
+Zend_Loader::loadClass('Zend_Registry');
+Zend_Loader::loadClass('Zend_View');
+Zend_Loader::loadClass('Zend_Config_Ini');
+Zend_Loader::loadClass('Zend_Db');
+Zend_Loader::loadClass('Zend_Db_Table');
+Zend_Loader::loadClass('Zend_Debug');
+Zend_Loader::loadClass('Zend_Auth');
+
+// load configuration
+$config = new Zend_Config_Ini('./application/config.ini', 'general');
+Zend_Registry::set('config', $config);
+
+// setup database
+$dbAdapter = Zend_Db::factory($config->db->adapter, 
+        $config->db->config->asArray());
+Zend_Db_Table::setDefaultAdapter($dbAdapter);
+Zend_Registry::set('dbAdapter', $dbAdapter);
+
+// setup controller
+$frontController = Zend_Controller_Front::getInstance();
+$frontController->throwExceptions(true);
+$frontController->setControllerDirectory('./application/controllers');
+
+// run!
+$frontController->dispatch();
