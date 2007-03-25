965e738a8be35a640ad3732305654b1c16906d44
Rob Allen <rob@akrabat.com>
2007-03-25 16:05:29 +0000

    Stage 2: Use of Zend_Auth

---
+++ stage2/application/lib/Controller/Plugin/Auth.php
@@ -0,0 +1,16 @@
+<?php
+
+class Controller_Plugin_Auth extends Zend_Controller_Plugin_Abstract
+{
+    public function preDispatch(Zend_Controller_Request_Abstract $request)
+    {
+        $registry = Zend_Registry::getInstance();
+        $authSession = $registry->get('authSession');
+        
+        if (!isset($authSession->user) && !isset($_POST['username']))
+        {
+            $request->setControllerName('auth');
+            $request->setActionName('login');
+        }
+    }
+}
