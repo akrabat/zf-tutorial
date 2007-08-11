7ebb25c666c25390a984f25fc1d33f29f0be8ead
rob <rob@f4f260f4-61fb-4e1b-9bfc-0e9184e17736>
2007-08-11 21:24:02 +0000

    add DB Schema

---
+++ trunk/stage1/application/dbschema.sql
@@ -0,0 +1,14 @@
+-- MySQL Database Schema
+
+CREATE TABLE album(
+	id int(11) NOT NULL AUTO_INCREMENT,
+	artist varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+
+INSERT INTO album (artist, title)
+VALUES (
+	'James Morrison', 'Undiscovered'), 
+	('Snow Patrol', 'Eyes Open');
