7dbc2be93ffe605d86d9cf2690b45a458e3d707d
rob <rob@f4f260f4-61fb-4e1b-9bfc-0e9184e17736>
2010-02-07 20:29:58 +0000

    progress

--- trunk/zf-tutorial/application/controllers/IndexController.php
+++ trunk/zf-tutorial/application/controllers/IndexController.php
@@ -10,9 +10,34 @@ class IndexController extends Zend_Controller_Action
 
     public function indexAction()
     {
+        $this->view->title = "My Albums";
+        $this->view->headTitle($this->view->title);
+
+        $albums = new Application_Model_DbTable_Albums();
+        $this->view->albums = $albums->fetchAll();
+    }
+
+    public function addAction()
+    {
+        // action body
+    }
+
+    public function editAction()
+    {
+        // action body
+    }
+
+    public function deleteAction()
+    {
         // action body
     }
 
 
 }
+
+
+
+
+
+
 
